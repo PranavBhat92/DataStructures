fa4afbd1786faa6615538fe90a377698f77267c1
PranavBhat <pranavbhat.1992@gmail.com>
2018-02-12 11:49:51 +0530

    DataStructures: Queues: Intermediate checkin

---
+++ DataStructures/Queues/ArrayQueues.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    public class AQueue<T> : IEnumerable<T>
+    {
+        T[] _items = new T[0];
+        int _size = 0;
+        int _head = 0;
+        int _tail = -1;        
+
+        public void Enqueue(T items)
+        {
+            
+        }
+
+        public T Dqueue()
+        {
+            T value = 
+        }
+
+        public T Peek() {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+
+
+    class ArrayQueues
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DataStructures/Queues/LinkedListQueue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    public class LQueue<T> : IEnumerable<T>
+    {
+
+        private LinkedList<T> _items = new LinkedList<T>();
+
+        public void EnQueue(T item)
+        {
+            _items.AddLast(item);
+        }
+
+
+        public T Dequeue()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var val = _items.First.Value;
+             _items.RemoveFirst();
+            return val;
+        }
+
+
+        public T Peek()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return _items.First.Value;
+        }
+
+        public int Count()
+        {
+            return _items.Count;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+
+
+    class LinkedListQueue
+    {
+    }
+}
